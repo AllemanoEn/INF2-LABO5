a7df809db89c5d9e77e6293025209ed9ede536cf
Enzo <enzo.allemano@hotmail.com>
2020-06-18 20:55:23 +0200

    2ème Maj du main

--- main.c
+++ main.c
@@ -13,14 +13,14 @@ bool checkFileExists(const char* filename){
 
 int main(int argc, char *argv[]) {
 
-    if(argc == 2 && !strcmp(argv[1], "-h"))
+    if(argc == 2 && !strcmp(argv[1],"-h"))
     {
-        // TODO: Afficher l'aide
+
         printf("-------------------------------------------------------------\n"
                "Pour utiliser cette executable, vous devez entrer 3 arguments\n\n"
                "arg 1 : chemin du fichier ou se trouve le texte.\n"
                "arg 2 : chemin du fichier de stockage du futur index.\n"
-               "arg 3 : chemin du fichier des mots à exclure."
+               "arg 3 : chemin du fichier des mots à exclure.\n"
                "-------------------------------------------------------------\n");
         return 0;
     }
